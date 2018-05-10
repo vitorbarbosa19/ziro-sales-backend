e87bc121e4458267a9134efe0263fabaa8aec817
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-05-09 18:04:02 -0700

    add new field and change dates from portuguese to english

--- functions/findMonth.js
+++ functions/findMonth.js
@@ -1,17 +1,17 @@
 const findMonth = (month) => {
 	switch (month) {
 		case 'Jan': return 01
-		case 'Fev': return 02
+		case 'Feb': return 02
 		case 'Mar': return 03
-		case 'Abr': return 04
-		case 'Mai': return 05
+		case 'Apr': return 04
+		case 'May': return 05
 		case 'Jun': return 06
 		case 'Jul': return 07
-		case 'Ago': return 08
-		case 'Set': return 09
-		case 'Out': return 10
+		case 'Aug': return 08
+		case 'Sep': return 09
+		case 'Oct': return 10
 		case 'Nov': return 11
-		case 'Dez': return 12
+		case 'Dec': return 12
 	}
 }
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', async (req, res) => {
 	const url = require('url')
 	const query = url.parse(req.url, true).query
 	// save query parameters from front-end form submit
-	const { boleto, lojista, fornecedor, pagamento, valor,
+	const { romaneio, boleto, lojista, fornecedor, pagamento, valor,
 		venda, comissao, assessor, vencimento, tipo } = query
 	// check if parameters are valid
 	const validParameters = Boolean(boleto && lojista && fornecedor && pagamento && valor && venda
@@ -18,11 +18,12 @@ app.get('/', async (req, res) => {
 		const findMonth = require('./functions/findMonth')
 		const receita = valor * comissao
 		const mes = findMonth(venda.substr(3,3))
+		const ano = venda.substr(7,4)
 		// save all parameters to a google spreadsheet
 		const sheetUpdater = require('./functions/sheetUpdater')
 		try {
-			res.send(await sheetUpdater({ boleto, lojista, fornecedor, pagamento, valor, venda, comissao,
-				assessor, vencimento, tipo, receita, mes }))
+			res.send(await sheetUpdater({ romaneio, boleto, lojista, fornecedor, pagamento, valor, venda,
+				comissao, assessor, vencimento, tipo, receita, mes }))
 		} catch (error) {
 			console.log(error)
 			res.send(error)
