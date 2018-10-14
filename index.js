e6f055454a9a3eb91a6c261cb927b8d2f5f0c397
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-10-14 14:58:34 -0700

    add address field

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express()
 
 app.get('/', async (req, res) => {
 	//set header to allow cross origin
-  res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Origin', '*')
 	const url = require('url')
 	const query = url.parse(req.url, true).query
 	// save query parameters from front-end form submit
 	const { romaneio, boleto, lojista, fornecedor, pagamento, valor,
-		venda, comissao, quantidade, assessor, vencimento, tipo } = query
+		venda, comissao, quantidade, assessor, vencimento, tipo, endereco } = query
 	// check if parameters are valid
 	const validParameters = Boolean(boleto && lojista && fornecedor && pagamento && valor && venda
-		&& comissao && quantidade && assessor && vencimento && tipo)
+		&& comissao && quantidade && assessor && vencimento && tipo && endereco)
 	// if valid, generate the calculated parameters
 	if (validParameters) {
 		const findMonth = require('./functions/findMonth')
@@ -24,9 +24,9 @@ app.get('/', async (req, res) => {
 		const sheetUpdater = require('./functions/sheetUpdater')
 		try {
 			await sheetUpdater({ sheet: 'ONE', romaneio, boleto, lojista, fornecedor, pagamento, valor, venda,
-				comissao, assessor, vencimento, quantidade, tipo, receita, mes, ano, cadastro })
+				comissao, assessor, vencimento, quantidade, tipo, endereco, receita, mes, ano, cadastro })
 			await sheetUpdater({ sheet: 'TWO', romaneio, boleto, lojista, fornecedor, pagamento, valor, venda,
-				comissao, assessor, vencimento, tipo, receita, mes, ano, cadastro })
+				comissao, assessor, vencimento, tipo, endereco, receita, mes, ano, cadastro })
 			res.send('_SUCCESS')
 		} catch (error) {
 			console.log(error)
